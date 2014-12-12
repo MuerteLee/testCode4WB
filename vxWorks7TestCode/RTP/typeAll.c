f59c70c65989dc0635e77fd2743a9b64c5a7cd6e
cli1 <cli1@windriver.com>
2014-12-12 17:25:50 +0800

    add test code for type part

---
+++ vxWorks7TestCode/RTP/typeAll.c
@@ -0,0 +1,52 @@
+/* includes */
+
+#include <stdio.h>
+
+char		v_char;
+signed char	v_signed_char;
+unsigned char	v_unsigned_char;
+
+short		v_short;
+signed short	v_signed_short;
+unsigned short	v_unsigned_short;
+
+int		v_int;
+signed int	v_signed_int;
+unsigned int	v_unsigned_int;
+
+long		v_long;
+signed long	v_signed_long;
+unsigned long	v_unsigned_long;
+
+float		v_float;
+double		v_double;
+
+
+int main ()
+{
+	extern void dummy();
+    dummy();
+    return 0;
+}
+
+void dummy()
+{
+  v_char = 'A';
+  v_signed_char = 'B';
+  v_unsigned_char = 'C';
+
+  v_short = 3;
+  v_signed_short = 4;
+  v_unsigned_short = 5;    
+
+  v_int = 6;
+  v_signed_int = 7;
+  v_unsigned_int = 8;    
+
+  v_long = 9;
+  v_signed_long = 10;
+  v_unsigned_long = 11;    
+  
+  v_float = 100.343434;
+  v_double = 200.565656;
+}
